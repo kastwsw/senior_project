b2b9b3664cbaa5fec27b84c840447425b4b89c45
Konstantin Kovalchuk <wsw@sol-digital.ru>
2024-04-29 18:25:09 +0300

    Draft design.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,6 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
 
     // Integration with activities
