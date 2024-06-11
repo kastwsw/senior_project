feb21fc7a9860b63828f3bfb88b9d2f667083d54
Konstantin Kovalchuk <wsw@sol-digital.ru>
2024-06-11 03:45:45 +0300

    Collect form.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.r3chain"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1000004
-        versionName = "1.0.4"
+        versionCode = 1000005
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
