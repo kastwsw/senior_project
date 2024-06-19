b182d1567fcf611f09d22e58414f37e965a2e7d4
Konstantin Kovalchuk <wsw@sol-digital.ru>
2024-06-19 12:22:19 +0300

    Arch.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.r3chain"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1000005
-        versionName = "1.0.5"
+        versionCode = 1000006
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
