c6bc976e89f749f5f3ddac0f636517e93d98941f
Konstantin Kovalchuk <wsw@sol-digital.ru>
2024-07-10 00:42:33 +0300

    Waste ui logic.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.r3chain"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1000007
-        versionName = "1.0.7"
+        versionCode = 1000008
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
